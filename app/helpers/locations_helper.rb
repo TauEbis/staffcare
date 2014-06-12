06c2d5c419dc49217278b4cfb976027c2ae3a2d7
Drew Blas <drew.blas@gmail.com>
2014-06-12 13:58:04 -0500

    Only show 6:00am to midnight

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -1,15 +1,15 @@
 module LocationsHelper
   def hours_options
     @_hours_options ||= begin
-      mins = [0,15,30,45]
+      m = 0
       buf = []
-      (0...24).each do |h|
-        mins.each do |m|
-          if h <= 12
-            buf << [sprintf("%02i:%02i am", h, m), h*60 + m]
-          else
-            buf << [sprintf("%02i:%02i pm", h-12, m), h*60 + m]
-          end
+      (6..24).each do |h|
+        if h == 24
+          buf << [sprintf("%02i:%02i am", h-12, m), h*60 + m]
+        elsif h <= 12
+          buf << [sprintf("%02i:%02i am", h, m), h*60 + m]
+        else
+          buf << [sprintf("%02i:%02i pm", h-12, m), h*60 + m]
         end
       end
 
