89226480d8187ef25df2801825f27bd4ac648503
Drew Blas <drew.blas@gmail.com>
2014-06-24 08:17:09 -0500

    Cleaned up loader

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -8,25 +8,19 @@ $(document).ready(function() {
     $(this.form).submit();
   });
 
-  $('#coverage_date_form').on('ajax:success', function(event, data, xhr, status){
-//    $('#coverage_view').removeClass('hidden');
-//    $('#coverage_view_load').addClass('hidden');
+  $('#coverage_date_form').on('ajax:before', function(event){
+    $('#coverage_view').addClass('hidden');
+    $('#coverage_view_load').removeClass('hidden');
+  })
+  .on('ajax:success', function(event, data, xhr, status){
+    $('#coverage_view').removeClass('hidden');
+    $('#coverage_view_load').addClass('hidden');
     $('#coverage_view').html(data);
+  })
+  .on('ajax:error', function(event, xhr, status, error){
+    $('#coverage_view').removeClass('hidden');
+    $('#coverage_view_load').addClass('hidden');
+    $('#coverage_view').text("ERROR: " + status + " - " + error);
+    console.log(xhr.responseText);
   });
-
-//  $('#coverage_date_form').on('ajax:before', function(event){
-//    $('#coverage_view').addClass('hidden');
-//    $('#coverage_view_load').removeClass('hidden');
-//  })
-//  .on('ajax:success', function(event, data, xhr, status){
-//    $('#coverage_view').removeClass('hidden');
-//    $('#coverage_view_load').addClass('hidden');
-//    $('#coverage_view').html(data);
-//  })
-//  .on('ajax:error', function(event, xhr, status, error){
-//    $('#coverage_view').removeClass('hidden');
-//    $('#coverage_view_load').addClass('hidden');
-//    $('#coverage_view').text("ERROR: " + status + " - " + error);
-//    console.log(xhr.responseText);
-//  });
 });
