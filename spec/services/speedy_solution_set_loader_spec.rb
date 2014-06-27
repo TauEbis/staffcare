68ce45647fa7754127ce23f3fa626fb25c2b0523
Rio Bennin <riobennin@gmail.com>
2014-06-27 00:06:15 -0400

    Add some  test files

---
+++ spec/services/speedy_solution_set_loader_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe SpeedySolutionSetLoader, :type => :service do
+	#let #    result = @loaded_solution_sets[key]
+#   @loaded_solution_sets.except!(key)
+#   result
+end
