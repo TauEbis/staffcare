ae91692952d6c4a7dc83e3a6c68dad70bcf5fed0
Drew Blas <drew.blas@gmail.com>
2014-07-02 16:28:23 -0500

    Fixing font path

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery.turbolinks
 //= require jquery_ujs
+//= require bootstrap-sprockets
 //= require bootstrap
 //= require bootstrap-datepicker/core
 //= require chosen.jquery.min
