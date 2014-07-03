832d54bc125397b38f794956711fc14ff2c6d143
Clay Collier <clay.collier@gmail.com>
2014-07-03 17:22:55 -0400

    Add migration for input volume projections table

---
+++ db/migrate/20140703154037_create_input_projections.rb
@@ -0,0 +1,10 @@
+class CreateInputProjections < ActiveRecord::Migration
+  def change
+    create_table :input_projections do |t|
+      t.date :start_date
+      t.date :end_date
+      t.string :location_name
+      t.int :volume
+    end
+  end
+end
