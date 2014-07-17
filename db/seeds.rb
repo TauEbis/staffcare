0a7b639ee847a1950c8d5623599e8724159e5e7e
Rio Bennin <riobennin@gmail.com>
2014-07-17 19:01:53 -0400

    Fix rake db:seed broken by Speed validation

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,21 +30,25 @@ if Rails.env.development?
   user.zones << z
   user.save!
 
-  z.locations.create!(name: "CityMD_14th_St", report_server_id: "CityMD_14th_St", max_mds: 3, rooms: 12, open_times: [9,8,8,8,8,8,9], close_times: [21,22,22,22,22,22,21])
-  z.locations.create!(name: "CityMD_23rd_St", report_server_id: "CityMD_23rd_St", max_mds: 3, rooms: 12, open_times: [9,8,8,8,8,8,9], close_times: [21,22,22,22,22,22,21])
-  z.locations.create!(name: "CityMD_57th_St", report_server_id: "CityMD_57th_St", max_mds: 3, rooms: 12, open_times: [9,8,8,8,8,8,9], close_times: [21,22,22,22,22,22,21])
+  z.locations.build(name: "CityMD_14th_St", report_server_id: "CityMD_14th_St", max_mds: 3, rooms: 12, open_times: [9,8,8,8,8,8,9], close_times: [21,22,22,22,22,22,21])
+  z.locations.build(name: "CityMD_23rd_St", report_server_id: "CityMD_23rd_St", max_mds: 3, rooms: 12, open_times: [9,8,8,8,8,8,9], close_times: [21,22,22,22,22,22,21])
+  z.locations.build(name: "CityMD_57th_St", report_server_id: "CityMD_57th_St", max_mds: 3, rooms: 12, open_times: [9,8,8,8,8,8,9], close_times: [21,22,22,22,22,22,21])
 
   z = Zone.create(name: 'NYC2')
 
-  z.locations.create!(name: "CityMD_67th_St", report_server_id: "CityMD_67th_St", max_mds: 3, rooms: 12, open_times: [9,8,8,8,8,8,9], close_times: [21,22,22,22,22,22,21])
-  z.locations.create!(name: "CityMD_86th_St", report_server_id: "CityMD_86th_St", max_mds: 3, rooms: 12, open_times: [9,8,8,8,8,8,9], close_times: [21,22,22,22,22,22,21])
-  z.locations.create!(name: "CityMD_88th_St", report_server_id: "CityMD_88th_St", max_mds: 3, rooms: 12, open_times: [9,8,8,8,8,8,9], close_times: [21,22,22,22,22,22,21])
+  z.locations.build(name: "CityMD_67th_St", report_server_id: "CityMD_67th_St", max_mds: 3, rooms: 12, open_times: [9,8,8,8,8,8,9], close_times: [21,22,22,22,22,22,21])
+  z.locations.build(name: "CityMD_86th_St", report_server_id: "CityMD_86th_St", max_mds: 3, rooms: 12, open_times: [9,8,8,8,8,8,9], close_times: [21,22,22,22,22,22,21])
+  z.locations.build(name: "CityMD_88th_St", report_server_id: "CityMD_88th_St", max_mds: 3, rooms: 12, open_times: [9,8,8,8,8,8,9], close_times: [21,22,22,22,22,22,21])
 
-  Location.all.each do |location|
-    location.speeds.create(doctors: 1, normal: 4, max: 6)
-    location.speeds.create(doctors: 2, normal: 8, max: 12)
-    location.speeds.create(doctors: 3, normal: 12, max: 18)
-    location.speeds.create(doctors: 4, normal: 16, max: 24)
-    location.speeds.create(doctors: 5, normal: 20, max: 30)
+
+  Zone.all.each do |zone|
+    zone.locations.each do |location|
+      location.speeds.create(doctors: 1, normal: 4, max: 6)
+      location.speeds.create(doctors: 2, normal: 8, max: 12)
+      location.speeds.create(doctors: 3, normal: 12, max: 18)
+      location.speeds.create(doctors: 4, normal: 16, max: 24)
+      location.speeds.create(doctors: 5, normal: 20, max: 30)
+      location.save
+    end
   end
 end
