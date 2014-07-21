76147ca40f1229abdc999d8e54d392349071767c
Clay Collier <clay.collier@gmail.com>
2014-07-21 16:39:31 -0400

    Report server connectivity
    Data query working
    
    Return body as parsed JSON

--- app/services/volume_history_importer.rb
+++ app/services/volume_history_importer.rb
@@ -1,25 +1,32 @@
+require 'json'
 
 class VolumeHistoryImporter
   include HTTParty
-  attr_reader :start_date, :end_date, :location, :session_id
+  attr_reader :start_date, :end_date, :session_id
+  attr_accessor :username, :password
   base_uri 'https://api.citymd.com/rio'
 
   USER_ENV_KEY = "CITYMD_API_REPORTS_USER"
   PASS_ENV_KEY = "CITYMD_API_REPORTS_PASSWORD"
 
+  BAD_IP_MESSAGE = "is not an authorized IP Address."
+  BAD_USERNAME_MESSAGE = "Invalid Login"
+  BAD_PASSWORD_MESSAGE = "Invalid login"
+
+  #debug_output $stderr
   # Create a new object to wrap a connection to the server
   # start_date - Beginning of date range to request data
   # end_date - End of " " " " "
   # locations - An array of location GUIDs or nil for all.
-  def initialize(start_date, end_date, location = nil)
-    @location ||= 'ALL'
+  def initialize(start_date, end_date, location)
+    location ||= 'ALL'
     # FIXME: These values should be 1) changed and 2) Not hard coded.
     @username = ENV[USER_ENV_KEY]||'Rio'
     @password = ENV[PASS_ENV_KEY]||'CitymdRio'
 
     if @username and @password
-      @options = { query: { startdate: start_date, enddate: end_date,
-                            servicesiteuid: @location } }
+      @options = { query: { startdate: start_date.to_s, enddate: end_date.to_s,
+                            servicesiteuid: location } }
       @session_id = nil
     else
       raise "No API password found in the environment! Set #{USER_ENV_KEY} and #{PASS_ENV_KEY} to correct this error."
@@ -27,37 +34,60 @@ class VolumeHistoryImporter
   end
 
   def self.valid_guid?(guid)
-    return /\h\h\h\h\h\h\h\h\-\h\h\h\h\-\h\h\h\h\-\h\h\h\h\-\h\h\h\h\h\h\h\h\h\h\h\h/.match(guid)
-  end
-
-  def authenticated?
-    if @session_id
+    if /^\h\h\h\h\h\h\h\h\-\h\h\h\h\-\h\h\h\h\-\h\h\h\h\-\h\h\h\h\h\h\h\h\h\h\h\h$/.match(guid) != nil
       return true
     else
       return false
     end
   end
 
-  def authenticate
-    response = self.class.get('/authenticate', { query: { username: @username, password: @password } })
-    if response.code == 200
-      if self.class.valid_guid?(response.body)
-        @sessiond_id = response.body
-        @options['sessionID'] = @session_id
-      else
-        raise "Authentication error- Failed to receive a valid session key from the server: #{response.body}"
-      end
+  def authenticated?
+    if @session_id != nil
+      return true
     else
-      puts response.code, response.message, response.headers.inspect
-      raise "Authentication error- Unable to authenticate to the remote server."
+      return false
     end
   end
 
-  def fetch_data
+  def authenticate!
+    response = self.class.get('/authenticate', { query: { username: @username, password: @password } })
+    if response.code == 200
+      if self.class.valid_guid?(response.body)
+        @session_id = response.body.chomp
+        @options[:query][:sessionID] = @session_id
+        @options[:headers] = { 'Cookie' => response.headers['Set-Cookie'] }
+      else
+        handle_auth_error(response.body)
+      end
+    else
+      raise IOError, "Connection error- got #{response.message} (#{response.code}) from server."
+    end
+  end
+
+  def fetch_data!
+    if self.session_id == nil
+      self.authenticate!()
+    end
     response = self.class.get('/patientload', @options)
-    # TODO: Check for problems
-    # TODO: Throw if anything goes wrong
-    return response.body
+    if response.code == 200
+      return JSON.parse(response.body)
+    else
+      raise IOError, "Error retrieving data- got #{response.message} (#{response.code}) from server."
+    end
+  end
+
+  private
+
+  def handle_auth_error(msg)
+    if msg.match(BAD_USERNAME_MESSAGE)
+      raise SecurityError, "Authentication error- Bad username."
+    elsif msg.match(BAD_PASSWORD_MESSAGE)
+      raise SecurityError, "Authentication error- Bad password."
+    elsif msg.match(BAD_IP_MESSAGE)
+      raise SecurityError, "Authentication error- IP is blocked."
+    else
+      raise SecurityError, "Authentication error- received #{msg} from server."
+    end
   end
 
 end
--- spec/services/volume_history_importer_spec.rb
+++ spec/services/volume_history_importer_spec.rb
@@ -8,17 +8,32 @@ describe VolumeHistoryImporter, :type => :service do
 # Attributes
   	it { should respond_to(:start_date) }
     it { should respond_to(:end_date) }
-    it { should respond_to(:location) }
-    it { should respond_to(:authenticate) }
-    it { should respond_to(:fetch_data) }
+    it { should respond_to(:authenticate!) }
+    it { should respond_to(:fetch_data!) }
     it { should respond_to(:authenticated?) }
+    it { should respond_to(:username) }
+    it { should respond_to(:password) }
 
-# Environmental dependencies
-    #
 # Class methods
   describe "#valid_guid?" do
     it "should recognize legal guids" do
-      expect(VolumeHistoryImporter.valid_guid?('414F0BD3-0460-405D-9136-0F16DB212BA9')).to be_truthy
+      expect(VolumeHistoryImporter.valid_guid?('414F0BD3-0460-405D-9136-0F16DB212BA9')).to eq true
+    end
+
+    it "should recognize an all-zero guid" do
+      expect(VolumeHistoryImporter.valid_guid?('00000000-0000-0000-0000-000000000000')).to eq true
+    end
+
+    it "should reject all numeric guids" do
+      expect(VolumeHistoryImporter.valid_guid?('414F0BD30460405D91360F16DB212BA9')).to eq false
+    end
+
+    it "should reject short guids" do
+      expect(VolumeHistoryImporter.valid_guid?('414F0BD3-0460-405D-9136-0F16DB212BA')).to eq false
+    end
+
+    it "should reject guids embedded in other text" do
+      expect(VolumeHistoryImporter.valid_guid?('hell414F0BD3-0460-405D-9136-0F16DB212BA9o')).to eq false
     end
   end
     
@@ -32,18 +47,55 @@ describe VolumeHistoryImporter, :type => :service do
     end
   end
 
-=begin
-  2014-07-18: Blocked by IP connectivity issues.
   describe "#authenticate" do
 
     context "when a correct username and password are provided" do
-      before { importer.authenticate() }
+      before { importer.authenticate!() }
       it "should establish a session when authenticate() is called" do
-        expect(importer.session_id).not_to be_nil
+        expect(importer.authenticated?).to eq true
+      end
+      it "should record the session id" do
+        expect(importer.session_id).not_to be_empty
+      end
+      it "should have a valid session id" do
+        expect(importer.class.valid_guid?(importer.session_id)).to eq true
+      end
+    end
+
+    context "when an invalid username is provided" do
+      before { importer.username = "oir" }
+      it "should raise an exception" do
+        expect{ importer.authenticate!() }.to raise_error(SecurityError, "Authentication error- Bad username.")
+      end
+    end
+
+    context "when an invalid password is provided" do
+      before { importer.password = "badpass" }
+      it "should raise an exception" do
+        expect{ importer.authenticate!() }.to raise_error(SecurityError, "Authentication error- Bad password.")
       end
     end
   end
-=end
+
+  describe "#fetch_data!" do
+
+    context "when called before authenticate" do
+      before { importer.fetch_data!() }
+      it "should call authenticate and create a session" do
+        expect(importer.authenticated?).to eq true
+      end
+    end
+
+    context "when called after authenticate" do
+      before { importer.authenticate!() }
+      it "should not authenticate again before fetching data" do
+        session = importer.session_id
+        importer.fetch_data!()
+        expect(session).to eq importer.session_id
+      end
+    end
+
+  end
 
 
 end
