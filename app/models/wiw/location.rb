e867160b054096e2298006102742797b4ad37acf
Drew Blas <drew.blas@gmail.com>
2014-07-23 15:10:49 -0500

    Edit locations to match with WIW

--- app/models/wiw/location.rb
+++ app/models/wiw/location.rb
@@ -1,12 +1,12 @@
 module Wiw
   class Location
     include HTTParty
-    base_uri 'https://api.wheniwork.com/2/shifts'
+    base_uri 'https://api.wheniwork.com/2/locations'
     headers 'W-Token' => Rails.application.secrets.wiw_token, 'Accept' => 'application/json', 'Content-Type' => 'application/json'
     format :json
 
     def self.find_all_collection
-      all.locations.map{|l| [l['name'], l['id']] }
+      get('/')['locations'].map{|l| [l['name'], l['id']] }
     end
   end
 end
