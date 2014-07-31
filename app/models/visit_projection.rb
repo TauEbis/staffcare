8da160237c0f10c26201c6aac1a8df52bc91153d
Drew Blas <drew.blas@gmail.com>
2014-07-31 10:57:50 -0500

    Sensicalize if statement

--- app/models/visit_projection.rb
+++ app/models/visit_projection.rb
@@ -46,10 +46,10 @@ class VisitProjection < ActiveRecord::Base
 
     schedule.days.each do |day|
       daily_vol = self.volumes[day.to_s]
-      unless source == :sample_run || source == :dummy_run
-        self.visits[day.to_s] = heat_maps.build_day_volume(daily_vol, day.wday)
-      else
+      if source == :sample_run || source == :dummy_run
         self.visits[day.to_s] = heat_maps[day.wday].map{ |percent| percent * daily_vol }
+      else
+        self.visits[day.to_s] = heat_maps.build_day_volume(daily_vol, day.wday)
       end
     end
 
