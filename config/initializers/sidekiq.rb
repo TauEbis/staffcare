5e868d14342003bc2f7a87b18f9838eac15f97b2
Drew Blas <drew.blas@gmail.com>
2014-10-14 09:57:13 -0500

    Update Sidekiq config with sane values for Heroku

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -17,6 +17,14 @@ Sidekiq.configure_server do |config|
     namespace: "staff_care_#{Rails.env}"
   }
 
+  if defined?(ActiveRecord::Base)
+    c = ActiveRecord::Base.configurations[Rails.env] ||
+                Rails.application.config.database_configuration[Rails.env]
+    c['pool']              = ENV['DB_POOL'] || 11
+    c['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
+    ActiveRecord::Base.establish_connection(c)
+  end
+
   config.server_middleware do |chain|
     chain.add Sidekiq::Status::ServerMiddleware, expiration: 1.day
   end
