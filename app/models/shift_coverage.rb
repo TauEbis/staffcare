cc8c007e2b2451fa50178c205c143832eb28cecb
Rio Bennin <riobennin@gmail.com>
2014-12-03 20:03:37 -0800

    Shift Coverage spec and refactor

--- app/models/shift_coverage.rb
+++ app/models/shift_coverage.rb
@@ -1,7 +1,8 @@
 class ShiftCoverage
 
-	SCORES 		= 					[0,0,0,0,0,0,0, 5, 10, 25, 30, 15, 20,  5,  10]
-#					hours in day:	[0,1,2,3,4,5,6,  7,  8,  9, 10, 11, 12, 13, 14] scores for each shift length
+				 	 	 #[0,1,2,3,4,5,6,  7,  8,  9, 10, 11, 12, 13, 14] scores for each shift length in hours
+	SCORES 		= [0,0,0,0,0,0,0, 5, 10, 25, 30, 15, 20,  5,  10]
+	MIN_SHIFT = 6
 
 # This method is called by the grade to recalculate coverage after manual shift changes
   def shifts_to_coverage(shifts)
@@ -13,8 +14,8 @@ class ShiftCoverage
 		coverage = Array.new((closes_at-opens_at) * 2, 0)
 		shifts.each do |s|
 			arrive = ( s.starts_hour - opens_at ) * 2
-			leave = ( s.ends_hour - closes_at ) * 2
-			coverage[arrive...leave] = coverage[arrive...leave].map{ |x| + 1 }
+			leave =  ( s.ends_hour - opens_at ) * 2
+			coverage[arrive...leave] = coverage[arrive...leave].map{ |x| x + 1 }
 		end
 
 		coverage
@@ -22,25 +23,22 @@ class ShiftCoverage
 
 # This method is only used for physician shifts currently -- All the methods beneath this are in the call chain of this method
   def coverage_to_shifts(coverage, location_plan, day)
-    @time = day.in_time_zone
+    time = day.in_time_zone
 		@opens_at = location_plan.open_times[day.wday]
-		@closes_at = location_plan.close_times[day.wday]
 
-		hours_open = @closes_at - @opens_at
-		@min_shift = [(hours_open * 0.5).round, 6].max
-		@midday = ( @opens_at + @closes_at )/2
+		hours_open = location_plan.close_times[day.wday] - @opens_at
+		@min_shift = [(hours_open * 0.5).round, MIN_SHIFT].max
 
   	shifts = pick_best(equiv_shifts_for(coverage))
 
     shifts.map do |starts_at, ends_at|
-      Shift.new(starts_at: @time + starts_at.hours, ends_at: @time + ends_at.hours )
+      Shift.new(starts_at: time + starts_at.hours, ends_at: time + ends_at.hours )
     end
   end
 
   def equiv_shifts_for(coverage)
-  	opens, closes = coverage_opens_closes(coverage)
-  	result = build_valid(opens, closes)
-  	result
+  	opens, closes = opens_closes(coverage)
+		build_valid(opens, closes)
   end
 
   def show_me(coverage)  # helper method for testing
@@ -70,64 +68,18 @@ class ShiftCoverage
 			set.map{ |shift| SCORES[ shift[1] - shift[0] ] }.sum / set.size.to_f
 		end
 
-		def coverage_opens_closes(coverage)
-			@coverage = coverage
-	    opens = shift_opens
-	    closes = shift_closes
+		def opens_closes(coverage)
+	    opens=[]
+	    closes=[]
 
-	    if ((diff = opens.size - closes.size) != 0)
-	      opens, closes = normalize(opens, closes, diff)
-	    end
+			steps = (coverage+[0]).zip([0] + coverage).map{ |x,y| x-y }
+			steps.each_with_index do |x, i|
+				opens  += [i/2 + @opens_at]*x 		if x>0
+				closes += [i/2 + @opens_at]*(-x)  if x<0
+			end
 
 	    return opens, closes
 		end
-
-	  def shift_opens
-	    morning = @coverage[0..(@coverage.size/2)]
-
-	    opens=Array.new(morning[0], @opens_at)
-
-	    (1..(morning.size-1)).each do |index|
-	      md_change = (morning[index] - morning[index-1])
-	      md_change.times do |y|
-	        hour = index/2 + @opens_at
-	        opens << hour
-	      end
-	    end
-
-	    opens
-	  end
-
-	  def shift_closes
-	    evening = @coverage[(@coverage.size/2 -1)..-1]
-
-	    closes = []
-
-	    (1..(evening.size-1)).each do |index|
-	      md_change = (evening[index-1] - evening[index])
-	      md_change.times do |y|
-	        hour =  @midday + (index-1)/2
-	        closes << hour
-	      end
-	    end
-
-	    evening[-1].times { |i| closes << @closes_at}
-
-	    closes
-	  end
-
-	# TODO: determine if this case occurs and test it
-	   def normalize(opens, closes, size_diff)
-
-	    if size_diff < 0
-	      size_diff.times { |i| opens << @midday }
-	    elsif size_diff > 0
-	      size_diff.times { |i| closes.unshift(@midday) }
-	    end
-
-	    return opens, closes
-
-	  end
 
 	  def build_valid(opens, closes)
 			result = []
@@ -136,6 +88,7 @@ class ShiftCoverage
 			end
 			result=result.uniq
 
+			# 'set' is a set of shifts: eg. [[8,22], [13,22], [10,22]]
 			result = result.reject do |set|
 				reject = false
 				set.each do |shift|
@@ -144,14 +97,19 @@ class ShiftCoverage
 				reject
 			end
 
-			found_splits = false
-			result.each do |set|
-				set.each do |shift|
-					found_splits = true if shift[1]-shift[0] == 2 * @min_shift
+			# Splits can not happen when day is open for an odd number of hours or when the min_shift is not half the day length
+			catch(:found_splits) do
+				result.each do |set|
+					set.each do |shift|
+						if shift[1]-shift[0] == 2 * @min_shift
+							midday = @opens_at + @min_shift
+							result = result + build_valid(opens+[midday], closes+[midday])
+							throw :found_splits
+						end
+					end
 				end
 			end
 
-			result = result + build_valid(opens+[@midday], closes+[@midday]) if found_splits
 			result
 		end
 
