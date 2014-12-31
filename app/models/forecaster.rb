7dac9a5ab5d73c931e6f1bdf50b0b255751cc21b
Rio Bennin <riobennin@gmail.com>
2014-12-31 09:13:17 +0100

    Forecaster WIP 2

--- app/models/forecaster.rb
+++ app/models/forecaster.rb
@@ -1,50 +1,97 @@
 class Forecaster
 #< ActiveRecord::Base
 =begin
-forecaster
-location_id
-ingest_id
-start date
-end date
-chunks = 2 (1/2 day)
-visits = array
-coeff = (hash of arrays (14 * 3) )
-origin = {mon_1: day}
 
-def forecast(date, chunk)
-	key= [#{date.day}_#{chunk}]
-	coeff = coeff[key]
-	origin = origin[key]
-	x = date-origin
+for migration:
 
-	coeff.each_with_index.map { |c, i| c * x^i }.sum
-end
+location_id reference
+ingest_id reference
+start_date date
+end_date date
+visits json
+degree integer
+chunks integer
+chunked_visits json
+origins json
+coeffs json
 
-def self.build
-	Forecaster.new(location, ingest)
-
-	set_origins(start_date, end_date)
-
-	visits = ingest.
-	chunked_visits = self.chunk(visits)
-
-	chunked_visits.each do |chunk|
-		coeff[:mon_0][0] = oly_avg(chunk)
-		coeff[:mon_0][1] = oly_avg(delta(chunk, 1))
-		coeef[:mon_0][2] = oly_avg(delta(chunk, 2))
-		coeef[:mon_0][3] = oly_avg(delta(chunk, 3))
-	end
-end
+belongs_to :location
+belongs_to :report_server_ingest
 =end
 
-def self.ol_avg(arry)
-	arry.map!(&:to_f) # do we want BigDecimals here?
-	min = arry.min
-	max = arry.max
-	arry.delete_at(arry.index(min))
-	arry.delete_at(arry.index(max))
+# returns the forecast for a chunk of a date
+def forecast(date, chunk)
+	key = "#{date.day.short_name}_#{chunk}".to_sym
 
+	x = (date-origins[key]).to_i
+	coeffs[key].each_with_index.map { |c, i| c * x^i }.sum # polynomial
+end
+
+def self.build_from_ingest(ingest)
+	chunks = 2 # hard coded to roughly am/pm (1/2 day)
+	degree = 3 # hard coded to cubic
+	start_date = ingest.start_date,
+	end_date = ingest.end_date,
+	locations = ingest.locations
+
+	locations.each do |location|
+		visits = ingest.get_visits(location)
+		chunked_visits = self.build_chunks(visits, degree)
+
+		attr = { location: location,
+						 ingest: ingest,
+						 start_date: start_date,
+						 end_date: end_date,
+						 visits: visits,
+						 chunks: chunks,
+						 degree: degree,
+						 chunked_visits: chunked_visits, # {mon_0: {date: summed_visits} }
+						 origins: self.build_origins(start_date, end_date, chunks), # {mon_1: date, mon_2: date}
+						 coeffs: self.build_coeffs(chunked_visits) #(hash of arrays (14 * 4) )
+						 }
+
+		Forecaster.new(attr)
+	end
+end
+
+def self.build_origins(start_date, end_date, chunks)
+	origins = {}
+	DAYS.EACH do |day|
+		day_num = day.dow
+		first_day = start_date.beginning_of_week + day_num.days
+		last_day = end_date.beginning_of_week + day_num.days
+		origin = (last_day + first_day)/2
+		(o...chunks).each{ |x| origins["#{day}_#{x}".to_sym] = origin }
+	end
+end
+
+def self.build_chunked_visits
+
+end
+
+# chunked_visits looks like {mon_0: [100, 200, 150, 125], mon_1: [100, 200, 150, 125], tues_0: [100, 200, 150, 125] ...}
+def self.build_coeffs(chunked_visits, degree)
+	coeffs={}
+	chunked_visits.each do |key, chunk|
+		coeffs[key] = (0..degree).map{ |d| oly_avg delta(chunk, d) }
+	end
+	coeffs
+end
+
+def self.oly_avg(arry)
+	arry.map!(&:to_f) # do we want BigDecimals here?
+	if arry.length > 2
+		min = arry.min  # Do I want abs value for min and max?
+		max = arry.max
+		arry.delete_at(arry.index(min))
+		arry.delete_at(arry.index(max))
+	end
 	arry.sum/(arry.length)
+end
+
+def self.delta(arry, degree)
+	diff = arry[1..-1].zip(arry[0..-2]).map { |x, y| x - y } if degree != 0
+	degree == 0 ? arry : self.delta(diff, degree-1)
 end
 
 end
