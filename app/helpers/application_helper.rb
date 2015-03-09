6cdd378d2094329424524f9cf53b40a38d02843b
Rio Bennin <riobennin@gmail.com>
2015-03-09 19:54:56 -0400

    Stop breadcrumbs from appearing on new schedule forms even if it is from a failed create attempt.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   def path_for_breadcrumbs?
     path = request.env['PATH_INFO']
-    path.match(/(schedules(\/\d)*|location_plans|grades)/)
+    (path.match(/(schedules(\/\d)+|location_plans|grades)/) || (path == schedules_path && params[:action] == 'index') ) && !path.include?('rules')
   end
 
   def zones_for_picker
